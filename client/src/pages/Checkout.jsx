516aa634f882355eb747629117a414a8acccd6cf
nurgul <nurguloksuzoglu@gmail.com>
2026-07-09 13:24:19 +0300

    checkout.jsx düzenlendi

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -307,11 +307,12 @@ export default function Checkout() {
                 </button>
               </div>
             )}
-            {!cartItems.some((i) => i.quantity >= 20) && (
-              <p className="coupon-hint">
+            {!cartItems.some((i) => i.quantity >= 20) &&
+              {
+                /*<p className="coupon-hint">
                 💡 Aynı üründen 20+ adet alımda kupon kullanabilirsiniz
-              </p>
-            )}
+              </p>*/
+              }}
           </div>
 
           <hr />
